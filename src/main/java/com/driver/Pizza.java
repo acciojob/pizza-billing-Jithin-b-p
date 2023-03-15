0995863a9f2c9da7263b30503083926aa37ecf93
unknown <jithinbp007@gmail.com>
2023-03-15 21:03:40 +0530

    pizza bill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -15,16 +15,17 @@ public class Pizza {
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        this.price = 0;
         this.paperBagAdded = false;
         this.extraCheeseAdded = false;
         this.extraToppingAdded = false;
         // your code goes here
         if(this.isVeg){
             this.price += 300;
-            this.bill = "Base Price Of The Pizza:"+ 300 + "\n";
+            this.bill = "Base Price Of The Pizza: "+ 300 + "\n";
         }else{
             this.price += 400;
-            this.bill = "Base Price Of The Pizza:"+ 400 + "\n";
+            this.bill = "Base Price Of The Pizza: "+ 400 + "\n";
         }
 
 
@@ -41,7 +42,7 @@ public class Pizza {
             return;
         }
         this.price += 80;
-        this.cheese = "Extra Cheese Added:" + 80 + "\n";
+        this.cheese = "Extra Cheese Added: " + 80 + "\n";
         extraCheeseAdded = true;
     }
 
@@ -52,10 +53,10 @@ public class Pizza {
         }
         if(this.isVeg){
             this.price += 70;
-            this.topping = "Extra Toppings Added:" + 70 + "\n";
+            this.topping = "Extra Toppings Added: " + 70 + "\n";
         }else{
             this.price += 120;
-            this.topping = "Extra Toppings Added:" + 120 + "\n";
+            this.topping = "Extra Toppings Added: " + 120 + "\n";
         }
         extraToppingAdded = true;
 
@@ -67,14 +68,14 @@ public class Pizza {
             return;
         }
         this.price += 20;
-        this.paperbag = "Paperbag Added:" + 20 + "\n";
+        this.paperbag = "Paperbag Added: " + 20 + "\n";
         paperBagAdded = true;
     }
 
     public String getBill(){
         // your code goes here
         this.bill += this.cheese + this.topping + this.paperbag;
-        this.bill += "Total Price:" + this.price + "\n";
+        this.bill += "Total Price: " + this.price + "\n";
 
         return this.bill;
 
